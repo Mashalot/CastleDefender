20cf918fde021caf10a6c5c5bb065ab569a5936f
Mashalot <jasonmatthew2352@gmail.com>
2017-09-26 17:21:52 -0500

    First

---
+++ CastleDefender/CastleDefender/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TreehouseDefense
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int DistanceTo(int x, int y)
+        {
+            int xDiff = X - x;
+            int yDiff = Y - y;
+
+            int xDiffSquared = xDiff * xDiff;
+            int yDiffSquared = yDiff * yDiff;
+
+            int xySum = xDiffSquared + yDiffSquared;
+            double distance = Math.Sqrt(xySum);
+            int distance1 = Convert.ToInt32(distance);
+            return distance1;
+        }
+    }
+}
